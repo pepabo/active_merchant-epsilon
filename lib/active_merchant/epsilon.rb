e7564d00db9076d12fc944d9f969ac13f9aa4c0b
Kenichi TAKAHASHI <kenichi.taka@gmail.com>
2014-09-08 15:44:27 +0900

    bundle gem active_merchant-epsilon

---
+++ lib/active_merchant/epsilon.rb
@@ -0,0 +1,7 @@
+require "active_merchant/epsilon/version"
+
+module ActiveMerchant
+  module Epsilon
+    # Your code goes here...
+  end
+end
---
+++ lib/active_merchant/epsilon/version.rb
@@ -0,0 +1,5 @@
+module ActiveMerchant
+  module Epsilon
+    VERSION = "0.0.1"
+  end
+end
