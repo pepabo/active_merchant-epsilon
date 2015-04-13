a6438bc35db5e02517fe50e45c6063b60b81d69c
Yuta Kurotaki <yuta.kurotaki@gmail.com>
2015-04-13 11:11:15 +0900

    Fix style

--- test/unit/gateways/epsilon_test.rb
+++ test/unit/gateways/epsilon_test.rb
@@ -1,7 +1,6 @@
 require 'test_helper'
 
 class EpsilonGatewayTest < MiniTest::Test
-
   def test_set_proxy_address_and_port
     ActiveMerchant::Billing::EpsilonGateway.proxy_address = 'http://myproxy.dev'
     ActiveMerchant::Billing::EpsilonGateway.proxy_port = 1234
