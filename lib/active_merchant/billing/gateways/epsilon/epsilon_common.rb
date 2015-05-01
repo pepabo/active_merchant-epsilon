b234b82ecb112fb424f304c69f4e4495b0ba77d8
INOUE Takuya <inouetakuya5@gmail.com>
2015-05-01 15:14:30 +0900

    initialize の super は inherited があるので不要だった

--- lib/active_merchant/billing/gateways/epsilon/epsilon_common.rb
+++ lib/active_merchant/billing/gateways/epsilon/epsilon_common.rb
@@ -11,7 +11,7 @@ module ActiveMerchant #:nodoc:
         CARD_NUMBER_MASK             = '//Epsilon_result/result[@card_number_mask]/@card_number_mask'
         CARD_BRAND                   = '//Epsilon_result/result[@card_brand]/@card_brand'
         ACS_URL                      = '//Epsilon_result/result[@acsurl]/@acsurl' # ACS (Access Control Server)
-        PA_REQ                       = '//Epsilon_result/result[@pareq]/@pareq' # PAReq (payment authentication request)
+        PA_REQ                       = '//Epsilon_result/result[@pareq]/@pareq' # PAReq (payer authentication request)
         RECEIPT_NUMBER               = '//Epsilon_result/result[@receipt_no][1]/@receipt_no'
         RECEIPT_DATE                 = '//Epsilon_result/result[@receipt_date][1]/@receipt_date'
         CONVENIENCE_STORE_LIMIT_DATE = '//Epsilon_result/result[@conveni_limit][1]/@conveni_limit'
@@ -28,9 +28,6 @@ module ActiveMerchant #:nodoc:
         base.cattr_accessor :contract_code, :proxy_address, :proxy_port
       end
 
-      def initialize(options = {})
-        super
-      end
 
       def authorize(money, payment, options = {})
         raise ActiveMerchant::Epsilon::InvalidActionError
