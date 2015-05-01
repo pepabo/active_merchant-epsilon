1d44b5b0e80cde51f124745a6e8ea4c920a4b7c9
INOUE Takuya <inouetakuya5@gmail.com>
2015-05-01 16:13:44 +0900

    不要になったエラークラスを削除した

--- lib/active_merchant/epsilon.rb
+++ lib/active_merchant/epsilon.rb
@@ -1,6 +1,5 @@
 require 'active_merchant'
 
-require_relative 'epsilon/errors'
 require_relative 'epsilon/version'
 
 require_relative 'billing/convenience_store'
--- lib/active_merchant/epsilon/errors.rb
+++
@@ -1,6 +0,0 @@
-module ActiveMerchant #:nodoc:
-  module Epsilon
-    class InvalidActionError < StandardError #:nodoc:
-    end
-  end
-end
