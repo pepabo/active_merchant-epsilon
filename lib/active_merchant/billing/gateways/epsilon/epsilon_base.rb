6b34abbbbb0d96628a410bfd2029d3e19a53d3b2
Joe-noh <goflb.jh@gmail.com>
2016-03-31 15:59:42 +0900

    get payment slip url from epsilon response
    response key is haraikomi_url

--- lib/active_merchant/billing/gateways/epsilon/epsilon_base.rb
+++ lib/active_merchant/billing/gateways/epsilon/epsilon_base.rb
@@ -30,17 +30,18 @@ module ActiveMerchant #:nodoc:
       self.abstract_class = true
 
       module ResponseXpath
-        RESULT                       = '//Epsilon_result/result[@result]/@result'
-        TRANSACTION_CODE             = '//Epsilon_result/result[@trans_code]/@trans_code'
-        ERROR_CODE                   = '//Epsilon_result/result[@err_code]/@err_code'
-        ERROR_DETAIL                 = '//Epsilon_result/result[@err_detail]/@err_detail'
-        CARD_NUMBER_MASK             = '//Epsilon_result/result[@card_number_mask]/@card_number_mask'
-        CARD_BRAND                   = '//Epsilon_result/result[@card_brand]/@card_brand'
-        ACS_URL                      = '//Epsilon_result/result[@acsurl]/@acsurl' # ACS (Access Control Server)
-        PA_REQ                       = '//Epsilon_result/result[@pareq]/@pareq' # PAReq (payer authentication request)
-        RECEIPT_NUMBER               = '//Epsilon_result/result[@receipt_no][1]/@receipt_no'
-        RECEIPT_DATE                 = '//Epsilon_result/result[@receipt_date][1]/@receipt_date'
-        CONVENIENCE_STORE_LIMIT_DATE = '//Epsilon_result/result[@conveni_limit][1]/@conveni_limit'
+        RESULT                             = '//Epsilon_result/result[@result]/@result'
+        TRANSACTION_CODE                   = '//Epsilon_result/result[@trans_code]/@trans_code'
+        ERROR_CODE                         = '//Epsilon_result/result[@err_code]/@err_code'
+        ERROR_DETAIL                       = '//Epsilon_result/result[@err_detail]/@err_detail'
+        CARD_NUMBER_MASK                   = '//Epsilon_result/result[@card_number_mask]/@card_number_mask'
+        CARD_BRAND                         = '//Epsilon_result/result[@card_brand]/@card_brand'
+        ACS_URL                            = '//Epsilon_result/result[@acsurl]/@acsurl' # ACS (Access Control Server)
+        PA_REQ                             = '//Epsilon_result/result[@pareq]/@pareq' # PAReq (payer authentication request)
+        RECEIPT_NUMBER                     = '//Epsilon_result/result[@receipt_no][1]/@receipt_no'
+        RECEIPT_DATE                       = '//Epsilon_result/result[@receipt_date][1]/@receipt_date'
+        CONVENIENCE_STORE_LIMIT_DATE       = '//Epsilon_result/result[@conveni_limit][1]/@conveni_limit'
+        CONVENIENCE_STORE_PAYMENT_SLIP_URL = '//Epsilon_result/result[@haraikomi_url][1]/@haraikomi_url'
       end
 
       module ResultCode
@@ -87,32 +88,34 @@ module ActiveMerchant #:nodoc:
         #   Nokogiri::XML::SyntaxError: Unsupported encoding x-sjis-cp932
         xml = Nokogiri::XML(body.sub('x-sjis-cp932', 'UTF-8'))
 
-        result                       = xml.xpath(ResponseXpath::RESULT).to_s
-        transaction_code             = xml.xpath(ResponseXpath::TRANSACTION_CODE).to_s
-        error_code                   = xml.xpath(ResponseXpath::ERROR_CODE).to_s
-        error_detail                 = uri_decode(xml.xpath(ResponseXpath::ERROR_DETAIL).to_s)
-        card_number_mask             = uri_decode(xml.xpath(ResponseXpath::CARD_NUMBER_MASK).to_s)
-        card_brand                   = uri_decode(xml.xpath(ResponseXpath::CARD_BRAND).to_s)
-        acs_url                      = uri_decode(xml.xpath(ResponseXpath::ACS_URL).to_s)
-        pa_req                       = uri_decode(xml.xpath(ResponseXpath::PA_REQ).to_s)
-        receipt_number               = xml.xpath(ResponseXpath::RECEIPT_NUMBER).to_s
-        receipt_date                 = uri_decode(xml.xpath(ResponseXpath::RECEIPT_DATE).to_s)
-        convenience_store_limit_date = uri_decode(xml.xpath(ResponseXpath::CONVENIENCE_STORE_LIMIT_DATE).to_s)
+        result                             = xml.xpath(ResponseXpath::RESULT).to_s
+        transaction_code                   = xml.xpath(ResponseXpath::TRANSACTION_CODE).to_s
+        error_code                         = xml.xpath(ResponseXpath::ERROR_CODE).to_s
+        error_detail                       = uri_decode(xml.xpath(ResponseXpath::ERROR_DETAIL).to_s)
+        card_number_mask                   = uri_decode(xml.xpath(ResponseXpath::CARD_NUMBER_MASK).to_s)
+        card_brand                         = uri_decode(xml.xpath(ResponseXpath::CARD_BRAND).to_s)
+        acs_url                            = uri_decode(xml.xpath(ResponseXpath::ACS_URL).to_s)
+        pa_req                             = uri_decode(xml.xpath(ResponseXpath::PA_REQ).to_s)
+        receipt_number                     = xml.xpath(ResponseXpath::RECEIPT_NUMBER).to_s
+        receipt_date                       = uri_decode(xml.xpath(ResponseXpath::RECEIPT_DATE).to_s)
+        convenience_store_limit_date       = uri_decode(xml.xpath(ResponseXpath::CONVENIENCE_STORE_LIMIT_DATE).to_s)
+        convenience_store_payment_slip_url = uri_decode(xml.xpath(ResponseXpath::CONVENIENCE_STORE_PAYMENT_SLIP_URL).to_s)
 
         {
-          success:                      [ResultCode::SUCCESS, ResultCode::THREE_D_SECURE].include?(result),
-          message:                      "#{error_code}: #{error_detail}",
-          transaction_code:             transaction_code,
-          error_code:                   error_code,
-          error_detail:                 error_detail,
-          card_number_mask:             card_number_mask,
-          card_brand:                   card_brand,
-          three_d_secure:               result == ResultCode::THREE_D_SECURE,
-          acs_url:                      acs_url,
-          pa_req:                       pa_req,
-          receipt_number:               receipt_number,
-          receipt_date:                 receipt_date,
-          convenience_store_limit_date: convenience_store_limit_date,
+          success:                            [ResultCode::SUCCESS, ResultCode::THREE_D_SECURE].include?(result),
+          message:                            "#{error_code}: #{error_detail}",
+          transaction_code:                   transaction_code,
+          error_code:                         error_code,
+          error_detail:                       error_detail,
+          card_number_mask:                   card_number_mask,
+          card_brand:                         card_brand,
+          three_d_secure:                     result == ResultCode::THREE_D_SECURE,
+          acs_url:                            acs_url,
+          pa_req:                             pa_req,
+          receipt_number:                     receipt_number,
+          receipt_date:                       receipt_date,
+          convenience_store_limit_date:       convenience_store_limit_date,
+          convenience_store_payment_slip_url: convenience_store_payment_slip_url
         }
       end
 
