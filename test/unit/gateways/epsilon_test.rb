5a16cc00460fb94d587987b63226e167ac7f816b
Ayami Sugimura <milkcaramel@gmail.com>
2017-05-12 11:33:18 +0900

    memo1、memo2のテストを追加

--- test/unit/gateways/epsilon_test.rb
+++ test/unit/gateways/epsilon_test.rb
@@ -20,6 +20,8 @@ class EpsilonGatewayTest < MiniTest::Test
       user_name: '山田太郎',
       item_name: 'すごい商品',
       order_number: '12345678',
+      memo1: '予備1',
+      memo2: '予備2',
     }
     gateway.expects(:ssl_post).with(
       instance_of(String),
@@ -27,6 +29,8 @@ class EpsilonGatewayTest < MiniTest::Test
         includes('user_name=%BB%B3%C5%C4%C2%C0%CF%BA'),
         includes('item_name=%A4%B9%A4%B4%A4%A4%BE%A6%C9%CA'),
         includes('order_number=12345678'),
+        includes('memo1=%CD%BD%C8%F71'),
+        includes('memo2=%CD%BD%C8%F72'),
       )
     ).returns(response_body)
     gateway.purchase(100, valid_credit_card, detail)
