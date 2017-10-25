6bd947fd7e95ecb652116a2a1b97938fff83d4a7
Yuta Kurotaki <yuta.kurotaki@gmail.com>
2017-10-25 10:25:55 +0900

    used token is invalid

--- test/remote/gateways/remote_epsilon_test.rb
+++ test/remote/gateways/remote_epsilon_test.rb
@@ -204,6 +204,14 @@ class RemoteEpsilonGatewayTest < MiniTest::Test
     end
   end
 
+  def test_token_verify_failure
+    # used token is invalid
+    VCR.use_cassette(:token_verify_failure) do
+      response = gateway.verify(tokenized_credit_card, purchase_detail_with_token)
+      assert_equal false, response.success?
+    end
+  end
+
   def test_find_user_success
     VCR.use_cassette(:find_user_success) do
       response = gateway.find_user(user_id: "U1416470209")
