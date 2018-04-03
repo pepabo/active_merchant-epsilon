73072000f031a5f5d21fa46161c51e9a7c621f31
ryuchan00 <leokun0210@gmail.com>
2018-04-03 20:08:25 +0900

    process_codeのモジュールもrequireしてあげる。

--- lib/active_merchant/epsilon.rb
+++ lib/active_merchant/epsilon.rb
@@ -4,6 +4,7 @@ require_relative 'epsilon/version'
 
 require_relative 'billing/convenience_store'
 require_relative 'billing/gateways/epsilon/epsilon_mission_code'
+require_relative 'billing/gateways/epsilon/epsilon_process_code'
 require_relative 'billing/gateways/epsilon/epsilon_base'
 require_relative 'billing/gateways/epsilon'
 require_relative 'billing/gateways/epslion_convenience_store'
