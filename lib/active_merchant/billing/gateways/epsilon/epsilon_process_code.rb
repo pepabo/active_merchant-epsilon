617c061690b4c45e7df178bb5438a1530b059e71
ryuchan00 <leokun0210@gmail.com>
2018-04-04 15:04:19 +0900

    delete account の方が表現としてわかりやすいので修正した。

--- lib/active_merchant/billing/gateways/epsilon/epsilon_process_code.rb
+++ lib/active_merchant/billing/gateways/epsilon/epsilon_process_code.rb
@@ -1,13 +1,13 @@
 module ActiveMerchant #:nodoc:
   module Billing #:nodoc:
     module EpsilonProcessCode
-      FIRST              = 1  # 初回課金
-      REGISTERED         = 2  # 登録済み課金
-      ONLY_REGISTER      = 3  # 登録のみ
-      CHANGE_INFORMATION = 4  # 登録内容変更
-      CANCEL_WITHDRAWAL  = 7  # 退会取り消し
-      CANCEL_MONTHLY     = 8  # 月次課金解除
-      WITHDRAWAL         = 9  # 退会
+      FIRST                 = 1  # 初回課金
+      REGISTERED            = 2  # 登録済み課金
+      ONLY_REGISTER         = 3  # 登録のみ
+      CHANGE_INFORMATION    = 4  # 登録内容変更
+      CANCEL_DELETE_ACCOUNT = 7  # 退会取り消し
+      CANCEL_MONTHLY        = 8  # 月次課金解除
+      DELETE_ACCOUNT        = 9  # 退会
     end
   end
 end
